48c40830346f8ed9e5cad0cd7084af5ffe7f7686
LanChi <lpham2525@gmail.com>
2020-12-17 18:55:28 -0500

    added root handlebars files

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-let ourRequest = new XMLHttpRequest()
-ourRequest.open('GET', 'https://learnweb.github.io/json-example/pets-data.json')
-ourRequest.onload = () => {
-  if (ourRequest.status >= 200 && ourRequest.status < 400) {
-    let data = JSON.parse(ourRequest.responseText)
-    createHTML(data)
-  } else {
-    console.log('We connected to the server, but it returned an error.')
-  }
-}
+// let ourRequest = new XMLHttpRequest()
+// ourRequest.open('GET', 'https://learnweb.github.io/json-example/pets-data.json')
+// ourRequest.onload = () => {
+//   if (ourRequest.status >= 200 && ourRequest.status < 400) {
+//     let data = JSON.parse(ourRequest.responseText)
+//     createHTML(data)
+//   } else {
+//     console.log('We connected to the server, but it returned an error.')
+//   }
+// }
 
-ourRequest.onerror = () => {
-  console.log('Connection error')
-}
+// ourRequest.onerror = () => {
+//   console.log('Connection error')
+// }
 
-ourRequest.send()
+// ourRequest.send()
 
-function createHTML(petsData) {
-  console.log('testing from our function')
-  console.log(petsData)
-}
+// function createHTML(petsData) {
+//   console.log('testing from our function')
+//   console.log(petsData)
+// }
